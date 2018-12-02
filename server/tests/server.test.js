d0478aa4074d4bd7cc3142564b825c6e599be72d
Sybille, Legitime <smlegitime@live.com>
2018-12-01 21:42:36 -0500

    Adding test suite for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,16 @@ const request = require('supertest');
 const {app} = require(path.join(__dirname, '..', 'server'));
 const {Todo} = require(path.join(__dirname, '..', 'models', 'todo'));
 
-// Making sure db is empty before every request
+const todos = [
+    { text: 'Premier test todo' },
+    { text: 'Second test todo' }
+];
+
+// Preparing db before every request
 beforeEach(done => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +32,7 @@ describe('POST /todos', () => {
         // Checking the db
         .end((err, res) => {
             if(err) { return done(err); }
-            Todo.find().then(todos => {
+            Todo.find({text}).then(todos => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -41,9 +48,21 @@ describe('POST /todos', () => {
         .end((err, res) => {
             if (err) { return done(err); }
             Todo.find().then(todos => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch(e => done(e));
         });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', done => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect(res => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     });
 });
